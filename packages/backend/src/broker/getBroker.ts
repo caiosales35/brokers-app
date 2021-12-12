0d180c4e539c2382a35d59cea84b94257bdd6e37
Caio Sales <caio.cesar@liven.tech>
2021-12-12 01:07:38 -0300

    fail 2

---
+++ packages/backend/src/broker/getBroker.ts
@@ -0,0 +1,8 @@
+// filtro por nome e telefone de broker
+// comemissoes /  maior -> menor
+// leads / maior -> menor
+export const getBrokerController = async () => {
+  // const brokers = await Lead.query().select()
+
+  return { oi: 'ou' }
+}
